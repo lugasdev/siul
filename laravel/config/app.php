4088d2504f8e68db31c4fad6da6a968cca93fd65
lugas <lugas.luqman@gmail.com>
2017-11-27 20:50:11 +0700

    apa ini

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://ulangan.local.id'),
 
     /*
     |--------------------------------------------------------------------------
